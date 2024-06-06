d1de5e81a8c721c92750b405be3061567b1217a6
WyngeNotFoundWasTaken <farki.kadric@gmail.com>
2024-06-06 12:40:59 +0200

    added some paths

--- myApp.js
+++ myApp.js
@@ -1,39 +1,67 @@
-let express = require('express');
+const bodyParser = require("body-parser");
+let express = require("express");
 let app = express();
+require("dotenv").config();
+
+
+console.log("Hello World");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+  
+app.use(function logReqs(req, res, next) {
+
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next();
+  
+})
+
+app.get('/now', function(req, res, next) {
+
+  req.time = new Date().toString();
+  next();
+  
+}, function(req, res) {
+
+  res.json({time: req.time});
+  
+})
+
+app.get('/:word/echo', function(req, res) {
+
+  res.json({echo: req.params.word});
+  
+});
+
+app.post('/name', function(req, res) {
+
+  res.json({name: req.body.first + " " + req.body.last})
+
+})
+
+app.get('/name', function(req, res) {
+
+  res.json({name: req.query.first + " " + req.query.last})
+  
+})
 
 
 
+app.get("/json", (req, res) => {
+  msg = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
+  
+  res.json({
+    message: msg,
+  });
+
+  
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
